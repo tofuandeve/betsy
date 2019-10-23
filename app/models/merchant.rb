3a178095ad405cf3d4b36fd88263064d4047b494
m.dove@live.com <m.dove@live.com>
2019-10-23 15:58:21 -0700

    Added Oauth login - no tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,13 @@
 class Merchant < ApplicationRecord
+    def self.build_from_github(auth_hash)
+        merchant = Merchant.new
+        merchant.uid = auth_hash[:uid]
+        merchant.provider = "github"
+        merchant.name = auth_hash["info"]["nickname"]
+        merchant.email = auth_hash["info"]["email"]
+    
+        # Note that the user has not been saved.
+        # We'll choose to do the saving outside of this method
+        return user
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "merchants#create"
   resources :merchants
 end
