2b1298df3f94eda98d4f0ac4b76c52158de412ef
Eve Le <nguyenthao.lee@gmail.com>
2019-10-23 22:33:41 -0700

    Update test_helper.rb and add test for homepages#index

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 describe HomepagesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "can get the index path" do
+      get root_path
+      
+      must_respond_with :success
+    end
+  end
 end
