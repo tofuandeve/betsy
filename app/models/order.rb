944f68d5bb44e6919f178688ba7cdc552cc0242d
Michaela Morrow <morrow.michaela@gmail.com>
2019-10-28 18:45:28 -0700

    Generated OrderItem model and established its relationships with Order and Product

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+    has_many :order_items
     validates :buyer_email, presence: true, on: :update
     validates :buyer_address, presence: true, on: :update
     validates :buyer_name, presence: true, on: :update
