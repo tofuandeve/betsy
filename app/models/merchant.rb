69c7c804643c75586cc2ffac8e674594ce6f8325
Michaela Morrow <morrow.michaela@gmail.com>
2019-10-31 13:57:58 -0700

    Defined and tested methods total_revenue, revenue_by_status, number_of_orders_by_status in merchant model; defined and tested item_subtotal method in order_item model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -14,6 +14,7 @@ class Merchant < ApplicationRecord
     return merchant
   end
   
+  # This also shows all of the merchant's orders if not given a parameter
   def orders_by_status(status = nil)
     orders = []
     
@@ -28,5 +29,52 @@ class Merchant < ApplicationRecord
     end
 
     return orders
+  end
+
+  def total_revenue
+    merchant_products = self.products
+
+    if merchant_products == nil
+      return 0
+    end
+
+    items = []
+    merchant_products.each do |product|
+      items << OrderItem.where(product_id: product.id)
+    end
+    items.flatten!
+
+    if items.empty?
+      return 0
+    end
+
+    earnings = []
+    items.each do |item|
+      earnings << item.item_subtotal
+    end
+
+    revenue = earnings.sum
+    return revenue
+  end
+
+  def revenue_by_status(status)
+    orders = self.orders_by_status(status)
+    earnings = []
+    orders.each do |order|
+      order.order_items.each do |item|
+        product = Product.find_by(id: item.product_id)
+        if product.merchant_id == self.id
+          earnings << item.item_subtotal
+        end
+      end
+    end
+
+    revenue = earnings.sum
+    return revenue
+  end
+
+  def number_of_orders_by_status(status)
+    orders = self.orders_by_status(status)
+    return orders.count
   end
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe Merchant do
   let(:valid_merchant1) { merchants(:merchant1) }
   let(:valid_merchant2) { merchants(:merchant2) }
-  
+  let(:valid_merchant3) { merchants(:merchant3) }
   
   before do
     @valid_orders = []
@@ -35,13 +35,40 @@ describe Merchant do
       valid_order_item.order = valid_order
       valid_order_item.save
     end
+
+    product = products(:product1)
+    @merchant3_product = Product.create(
+      name: product.name,
+      status: product.status,
+      description: product.description,
+      price: product.price,
+      stock: product.stock,
+      photo_url: product.photo_url,
+      merchant_id: valid_merchant3.id 
+    )
+    @merchant3_order = Order.create(
+      buyer_email: "raccon@raccoon.net",
+      buyer_address: "123 raccoon way",
+      buyer_name: "Rocky Raccoon Jr.",
+      buyer_card: "12334577848",
+      card_expiration: "12/23",
+      cvv: "234",
+      zipcode: "98102"
+    )
+
+    @merchant3_order_item = OrderItem.new(quantity: 1)
+    @merchant3_order_item.product = @merchant3_product
+    @merchant3_order_item.order = @merchant3_order
+    @merchant3_order_item.save
   end
   
-  describe "orders method" do
-    it "returns a list of all Order instances" do
+  describe "order by status method" do
+    it "returns a list of all Order instances for the given merchant and does not return orders belonging to other merchants" do
       orders = valid_merchant1.orders_by_status
+      merchant3_orders = valid_merchant3.orders_by_status
       
-      expect _(orders.length).must_equal Order.count
+      expect _(orders.length).must_equal 3
+      expect(merchant3_orders.length).must_equal 1
       expect(orders).must_be_instance_of Array
       orders.each do |order|
         expect(order).must_be_instance_of Order
@@ -69,6 +96,42 @@ describe Merchant do
       
       expect(orders).must_be_instance_of Array
       assert(orders.empty?)
+    end
+  end
+
+  describe "total_revenue" do
+    it "returns the total revenue when a merchant has multiple valid orderitems" do
+      expect(valid_merchant1.total_revenue).must_equal 142.0
+    end
+
+    it "returns 0 when the merchant doesn't have any products" do
+      expect(valid_merchant2.total_revenue).must_equal 0
+    end
+  end
+
+  describe "revenue_by_status" do
+    it "returns the total revenue of all the merchant's orders with the given valid status" do
+      @valid_orders.each do |order|
+        expect(order.status).must_equal "pending"
+      end
+      expect(valid_merchant1.revenue_by_status("pending")).must_equal 142
+    end
+
+    it "returns 0 if the merchant does not have any orders with the given status" do
+      expect(valid_merchant1.revenue_by_status("paid")).must_equal 0
+    end
+  end
+
+  describe "number_of_orders_by_status" do
+    it "returns the total number of the merchant's orders with the given status" do
+      @valid_orders.each do |order|
+        expect(order.status).must_equal "pending"
+      end
+      expect(valid_merchant1.number_of_orders_by_status("pending")).must_equal 3
+    end
+
+    it "returns 0 if the merchant does not have any orders with the given status" do
+      expect(valid_merchant1.number_of_orders_by_status("paid")).must_equal 0
     end
   end
 end
