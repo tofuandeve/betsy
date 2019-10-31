5b5fa5644c9f53658853925cb801255801f00509
m.dove@live.com <m.dove@live.com>
2019-10-31 14:22:26 -0700

    Added button for review on products show and added format date helper view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def readable_date(date)
+    return "[unknown]" unless date
+    return ("<span class='date' title='".html_safe +
+            date.to_s +
+            "'>".html_safe +
+            time_ago_in_words(date) +
+            " ago</span>".html_safe)
+  end
 end
