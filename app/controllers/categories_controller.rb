a2f00e22d70929f862c592e55f3fe9d281cd114e
Eve Le <nguyenthao.lee@gmail.com>
2019-10-31 16:41:20 -0700

    Updated test and action for create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,27 +7,27 @@ class CategoriesController < ApplicationController
     end
     @category = Category.new
   end
-
+  
   def create
     if session[:merchant_id] == nil
       flash[:error] = "Please log in as a merchant to create a category."
       redirect_to root_path
       return
     end
-
+    
     @category = Category.new(category_params)
-
+    
     if @category.save
       flash[:success] = "Successfully created new category."
-      redirect_to merchant_path(session[:merchant_id])
+      redirect_to root_path
     else
       flash.now[:error] = "Invalid entry. Please try again."
       render :new
     end
   end
-
+  
   private
-
+  
   def category_params
     return params.require(:category).permit(:name)
     #do we need to include the product_ids?
