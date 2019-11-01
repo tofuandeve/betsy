732a684f7472df6fb70e4a510abc9bc4fab0cbc8
Erika <erika.maust@gmail.com>
2019-10-31 19:20:26 -0700

    Merge conflict resolution A+ experience

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -29,4 +29,25 @@ class Merchant < ApplicationRecord
 
     return orders
   end
+
+  def revenue_by_status(status = nil)
+    orders = self.orders_by_status(status)
+    earnings = []
+    orders.each do |order|
+      order.order_items.each do |item|
+        product = Product.find_by(id: item.product_id)
+        if product.merchant_id == self.id
+          earnings << item.item_subtotal
+        end
+      end
+    end
+    revenue = earnings.sum
+    return revenue
+  end
+  
+  def number_of_orders_by_status(status)
+    orders = self.orders_by_status(status)
+    return orders.count
+  end
+
 end
