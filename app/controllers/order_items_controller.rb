1e021370b9f0761dcacb71869cf0ec19f9d7b00c
Eve Le <nguyenthao.lee@gmail.com>
2019-10-31 19:37:56 -0700

    Fix flash message when adding product in to cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,14 +1,14 @@
 class OrderItemsController < ApplicationController
   before_action :find_order, only: [:create, :update]
-
+  
   def create
     if @order.nil?
       @order = Order.create
       session[:order_id] = @order.id
     end
-
+    
     @product = Product.find_by(id: params[:id])
-
+    
     if @product.nil?
       flash[:error] = "Product doesn't exist!"
       redirect_to products_path
@@ -17,10 +17,13 @@ class OrderItemsController < ApplicationController
       flash[:error] = "#{@product.name} is not available!"
     else
       @order_item = @order.find_order_item_by_product_id(@product.id)
-
+      
       if @order_item
         if @order_item.quantity < @product.stock
           @order_item.quantity += 1
+          flash[:success] = "Item was successfully added to cart!"
+        else
+          flash[:error] = "Not enough stock to add more to your cart!"
         end
       else
         @order_item = OrderItem.new(
@@ -28,34 +31,33 @@ class OrderItemsController < ApplicationController
           order_id: session[:order_id],
           quantity: 1,
         )
-      end
-
-      if @order_item.save
         flash[:success] = "Item was successfully added to cart!"
-      else
+      end
+      
+      if !@order_item.save
         flash[:error] = "Failed to add item to cart!"
       end
     end
-
+    
     redirect_to product_path(@product.id)
     return
   end
-
+  
   def update
     if @order.nil?
       redirect_to products_path
       return
     end
-
+    
     @order_item = OrderItem.find_by(id: params[:id])
-
+    
     if @order_item && @order_item.order_id == @order.id
       quantity_difference = @order_item.quantity_change(@order_item.order, params[:order_item][:quantity])
       @product = @order_item.product
-
+      
       if @product.update_stock(quantity_difference)
         @order_item.quantity = params[:order_item][:quantity]
-
+        
         if @order_item.save
           flash[:success] = "Item in cart was successfully updated"
         else
@@ -65,24 +67,24 @@ class OrderItemsController < ApplicationController
         flash[:error] = "There aren't enough items in stock for your order!"
       end
     end
-
+    
     redirect_to order_path(@order.id)
     return
   end
-
+  
   def destroy
     order_item = OrderItem.find_by(id: params[:id])
     if order_item
       order_item.destroy
       flash[:success] = "Item: " + order_item.product.name + " was successfully deleted!"
     end
-
+    
     redirect_to order_path
     return
   end
-
+  
   private
-
+  
   def find_order
     @order = Order.find_by(id: session[:order_id])
   end
